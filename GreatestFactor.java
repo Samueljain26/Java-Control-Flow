5b2a92833c062cc0ff4a8aab859bfb13a7e5946b
Samueljain26 <samueljain77@gmail.com>
2025-03-02 23:59:50 +0530

    [Samuel] ADD: Finding the greatest factor of a number using for loop

---
+++ GreatestFactor.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class GreatestFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            int greatestFactor = 1;
+            for (int i = number - 1; i >= 1; i--) {
+                if (number % i == 0) {
+                    greatestFactor = i;
+                    break;
+                }
+            }
+            System.out.println("Greatest factor is: " + greatestFactor);
+        }
+	 else {
+            System.out.println("Not a positive integer");
+        }
+    }
+}
